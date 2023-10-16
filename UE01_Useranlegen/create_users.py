6893b383854b7ecdb807a55bf7e9b5fdd6d2b509
Karanbir Guron <karanbir.GURON@htl.rennweg.at>
2023-10-16 15:29:00 +0200

    UE01: create_users.py main

--- UE01_Useranlegen/create_users.py
+++ UE01_Useranlegen/create_users.py
@@ -1,5 +1,7 @@
 import unicodedata
 
+from openpyxl.reader.excel import load_workbook
+
 
 def shave_marks(txt):
     """Remove all diacritic marks"""
@@ -8,3 +10,9 @@ def shave_marks(txt):
                      if not unicodedata.combining(c))
     erg = unicodedata.normalize('NFC', shaved)
     return erg
+
+
+
+if __name__ == '__main__':
+    wb = load_workbook("Namen.xlsx", read_only=True)
+    ws = wb[wb.sheetnames[0]]
