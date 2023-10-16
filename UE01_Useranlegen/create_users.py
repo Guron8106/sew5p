65ed821e63c251510356d94dd614f5e4ea6e65ff
Karanbir Guron <karanbir.GURON@htl.rennweg.at>
2023-10-16 15:50:00 +0200

    UE01: create_users.py generate_password

--- UE01_Useranlegen/create_users.py
+++ UE01_Useranlegen/create_users.py
@@ -1,3 +1,5 @@
+import random
+import string
 import unicodedata
 
 from openpyxl.reader.excel import load_workbook
@@ -11,6 +13,14 @@ def shave_marks(txt):
     erg = unicodedata.normalize('NFC', shaved)
     return erg
 
+def generate_password():
+    """Generates Password for a user
+    for _ in range(10): This part creates a loop that repeats 10 times (range(10)). During each iteration,
+     a random letter is chosen.
+
+    """
+    password_chars = string.ascii_letters + "!%&(),._-=^#!%&(),._-=^#"
+    return ''.join(random.choice(password_chars) for _ in range(10))
 
 
 if __name__ == '__main__':
