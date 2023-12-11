260fce4fd4ac415f68614220ad2e611f62820643
Karanbir Guron <karanbir.GURON@htl.rennweg.at>
2023-12-11 16:55:00 +0100

    UE01: Logfiles und Bereit zur Abnahme

---
+++ UE01_Useranlegen/res/create_user.sh
@@ -0,0 +1,40 @@
+set -e
+useradd -d "/home/de junious" -c "maximilian de junious" -m -g student,2CN -s "/bin/bash de junious" && echo de junious:"&)!Q%k)^dW" | chpasswd
+useradd -d "/home/nunez gomez" -c "ramon nunez gomez" -m -g student,3BN -s "/bin/bash nunez gomez" && echo nunez gomez:"FSmHT#.((b" | chpasswd
+useradd -d "/home/uellaegoess" -c "ulku omer uellaegoess" -m -g student,1AN -s "/bin/bash uellaegoess" && echo uellaegoess:"F^na.F%Bg(" | chpasswd
+useradd -d "/home/lanpher" -c "isis lanpher" -m -g student,4CN -s "/bin/bash lanpher" && echo lanpher:",vx_(.Nnlp" | chpasswd
+useradd -d "/home/galvin" -c "maximilian galvin" -m -g student,2AN -s "/bin/bash galvin" && echo galvin:"zbkJq)VZjE" | chpasswd
+useradd -d "/home/laspina" -c "jannette laspina" -m -g student,4CN -s "/bin/bash laspina" && echo laspina:"J)zofD==Re" | chpasswd
+useradd -d "/home/dodich" -c "marg dodich" -m -g student,3CN -s "/bin/bash dodich" && echo dodich:"_)zm)jD(&D" | chpasswd
+useradd -d "/home/bannowsky" -c "carisa bannowsky" -m -g student,2AN -s "/bin/bash bannowsky" && echo bannowsky:"Cd,O^dgEhP" | chpasswd
+useradd -d "/home/waisath" -c "david waisath" -m -g student,4CN -s "/bin/bash waisath" && echo waisath:"&&X#((xug_" | chpasswd
+useradd -d "/home/von reddig-piette" -c "paulette von reddig-piette" -m -g student,2CN -s "/bin/bash von reddig-piette" && echo von reddig-piette:"!qD.sx!q!L" | chpasswd
+useradd -d "/home/latona" -c "kirby latona" -m -g student,2AN -s "/bin/bash latona" && echo latona:"FGd=n#A)H&" | chpasswd
+useradd -d "/home/homewood" -c "reed homewood" -m -g student,2BN -s "/bin/bash homewood" && echo homewood:"=xXiQ(FzP^" | chpasswd
+useradd -d "/home/pallafor-zedian" -c "blair pallafor-zedian" -m -g student,5CN -s "/bin/bash pallafor-zedian" && echo pallafor-zedian:"D#VNqnV%jC" | chpasswd
+useradd -d "/home/senemounnarat-quillian" -c "lon senemounnarat-quillian" -m -g student,2CN -s "/bin/bash senemounnarat-quillian" && echo senemounnarat-quillian:"b&sDId(qgu" | chpasswd
+useradd -d "/home/isaac" -c "vada isaac" -m -g student,2BN -s "/bin/bash isaac" && echo isaac:"_gz_AMihJ(" | chpasswd
+useradd -d "/home/plancarte" -c "jeanett plancarte" -m -g student,4BN -s "/bin/bash plancarte" && echo plancarte:")yplqB%#I%" | chpasswd
+useradd -d "/home/berteotti-stirn" -c "alex berteotti-stirn" -m -g teacher -s "/bin/bash berteotti-stirn" && echo berteotti-stirn:"Au,uNKW)lS" | chpasswd
+useradd -d "/home/strycker" -c "robyn strycker" -m -g student,3AN -s "/bin/bash strycker" && echo strycker:"C(Yp_kHwQY" | chpasswd
+useradd -d "/home/von verrill" -c "camille von verrill" -m -g student,3BN -s "/bin/bash von verrill" && echo von verrill:"k&nsw&eGC=" | chpasswd
+useradd -d "/home/deschner" -c "franz michael leopold deschner" -m -g student,4CN -s "/bin/bash deschner" && echo deschner:"BPzH#fMhLn" | chpasswd
+useradd -d "/home/franzi" -c "veola franzi" -m -g teacher -s "/bin/bash franzi" && echo franzi:"agOdGDMznY" | chpasswd
+useradd -d "/home/cringle" -c "chantelle cringle" -m -g student,3CN -s "/bin/bash cringle" && echo cringle:"h-b_sctzBs" | chpasswd
+useradd -d "/home/kosh" -c "britney kosh" -m -g student,5BN -s "/bin/bash kosh" && echo kosh:"wD-g.Fujqc" | chpasswd
+useradd -d "/home/derouchie" -c "clayton derouchie" -m -g student,4BN -s "/bin/bash derouchie" && echo derouchie:"Z,F.BT!LG&" | chpasswd
+useradd -d "/home/doutt" -c "beverlee doutt" -m -g student,5BN -s "/bin/bash doutt" && echo doutt:"ni==QLLKN&" | chpasswd
+useradd -d "/home/munley" -c "alma munley" -m -g student,1BN -s "/bin/bash munley" && echo munley:"yxoTo^EyR%" | chpasswd
+useradd -d "/home/dornbos" -c "thad dornbos" -m -g student,5AN -s "/bin/bash dornbos" && echo dornbos:"wjqa,.G!,_" | chpasswd
+useradd -d "/home/mahala" -c "arvilla mahala" -m -g student,2CN -s "/bin/bash mahala" && echo mahala:"V=^^ikU,_B" | chpasswd
+useradd -d "/home/ellingwood" -c "mirza ellingwood" -m -g teacher -s "/bin/bash ellingwood" && echo ellingwood:"gVPQ%-o!Uc" | chpasswd
+useradd -d "/home/de cardinalli-sciola" -c "francie de cardinalli-sciola" -m -g student,3AN -s "/bin/bash de cardinalli-sciola" && echo de cardinalli-sciola:"u(HPQcZ.o#" | chpasswd
+useradd -d "/home/gundry" -c "irenee gundry" -m -g teacher -s "/bin/bash gundry" && echo gundry:",_!TT^mvq," | chpasswd
+useradd -d "/home/pinedo" -c "irenee pinedo" -m -g student,3CN -s "/bin/bash pinedo" && echo pinedo:"K,_M(%DswG" | chpasswd
+useradd -d "/home/faivre" -c "chel'ah faivre" -m -g student,2CN -s "/bin/bash faivre" && echo faivre:"BeONF%hKPa" | chpasswd
+useradd -d "/home/pinsky" -c "mirza pinsky" -m -g student,1BN -s "/bin/bash pinsky" && echo pinsky:"t-%Bp^n%G=" | chpasswd
+useradd -d "/home/pinsky1" -c "francie pinsky" -m -g student,1BN -s "/bin/bash pinsky1" && echo pinsky1:"q-Ac_zJ-bQ" | chpasswd
+useradd -d "/home/pinsky2" -c "goldie pinsky" -m -g student,1BN -s "/bin/bash pinsky2" && echo pinsky2:"E-V&SXo),f" | chpasswd
+useradd -d "/home/pinsky3" -c "a-nother pinsky" -m -g student,1BN -s "/bin/bash pinsky3" && echo pinsky3:"M,n,PunauQ" | chpasswd
+useradd -d "/home/norlien" -c "ανδρομεδη norlien" -m -g student,2AN -s "/bin/bash norlien" && echo norlien:"M,!NFFxoP(" | chpasswd
+useradd -d "/home/none" -c "none none" -m -g None -s "/bin/bash none" && echo none:"A&YUZbMWd_" | chpasswd
---
+++ UE01_Useranlegen/res/delete_user.sh
@@ -0,0 +1,40 @@
+set -x
+userdel de junious && rm -rf /home/klassen/de junious
+userdel nunez gomez && rm -rf /home/klassen/nunez gomez
+userdel uellaegoess && rm -rf /home/klassen/uellaegoess
+userdel lanpher && rm -rf /home/klassen/lanpher
+userdel galvin && rm -rf /home/klassen/galvin
+userdel laspina && rm -rf /home/klassen/laspina
+userdel dodich && rm -rf /home/klassen/dodich
+userdel bannowsky && rm -rf /home/klassen/bannowsky
+userdel waisath && rm -rf /home/klassen/waisath
+userdel von reddig-piette && rm -rf /home/klassen/von reddig-piette
+userdel latona && rm -rf /home/klassen/latona
+userdel homewood && rm -rf /home/klassen/homewood
+userdel pallafor-zedian && rm -rf /home/klassen/pallafor-zedian
+userdel senemounnarat-quillian && rm -rf /home/klassen/senemounnarat-quillian
+userdel isaac && rm -rf /home/klassen/isaac
+userdel plancarte && rm -rf /home/klassen/plancarte
+userdel berteotti-stirn && rm -rf /home/klassen/berteotti-stirn
+userdel strycker && rm -rf /home/klassen/strycker
+userdel von verrill && rm -rf /home/klassen/von verrill
+userdel deschner && rm -rf /home/klassen/deschner
+userdel franzi && rm -rf /home/klassen/franzi
+userdel cringle && rm -rf /home/klassen/cringle
+userdel kosh && rm -rf /home/klassen/kosh
+userdel derouchie && rm -rf /home/klassen/derouchie
+userdel doutt && rm -rf /home/klassen/doutt
+userdel munley && rm -rf /home/klassen/munley
+userdel dornbos && rm -rf /home/klassen/dornbos
+userdel mahala && rm -rf /home/klassen/mahala
+userdel ellingwood && rm -rf /home/klassen/ellingwood
+userdel de cardinalli-sciola && rm -rf /home/klassen/de cardinalli-sciola
+userdel gundry && rm -rf /home/klassen/gundry
+userdel pinedo && rm -rf /home/klassen/pinedo
+userdel faivre && rm -rf /home/klassen/faivre
+userdel pinsky && rm -rf /home/klassen/pinsky
+userdel pinsky1 && rm -rf /home/klassen/pinsky1
+userdel pinsky2 && rm -rf /home/klassen/pinsky2
+userdel pinsky3 && rm -rf /home/klassen/pinsky3
+userdel norlien && rm -rf /home/klassen/norlien
+userdel none && rm -rf /home/klassen/none
