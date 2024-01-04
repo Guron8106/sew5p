43f12b899aa8d514f1b5243acd7ca8d727e110b3
Karanbir Guron <karanbir.GURON@htl.rennweg.at>
2024-01-04 15:11:00 +0100

    UE02: suche v0

--- UE02_labyrinth/Labyrinth.py
+++ UE02_labyrinth/Labyrinth.py
@@ -79,3 +79,38 @@ def printLabyrinth(lab):
     """
     for row in lab:
         print("".join(row))
+
+
+def suche(zeile, spalte, lab):
+    """
+    Suche Ausgang
+
+    :param zeile: Start-Pos X
+    :param spalte: Start-Pos Y
+    :param lab: Labyrinth
+    :return: Boolean 1 or 0
+    """
+    if zeile < 0 or zeile >= len(lab) or spalte < 0 or spalte >= len(lab[zeile]) or lab[zeile][spalte] == '#' or \
+            lab[zeile][spalte] == '.' or lab[zeile][spalte] == 'X':
+        return 0
+
+    if lab[zeile][spalte] == 'A':
+        return 1
+
+    lab[zeile][spalte] = '.'
+
+
+    printLabyrinth(lab)
+
+
+    if (suche(zeile + 1, spalte, lab) or
+            suche(zeile - 1, spalte, lab) or
+            suche(zeile, spalte + 1, lab) or
+            suche(zeile, spalte - 1, lab)):
+        return 1
+
+    lab[zeile][spalte] = ' '
+
+    return 0
+
+
