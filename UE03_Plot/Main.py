e439f6cfbd9aaaf5f6e8e34e8f4b921b8cdd2f15
Karanbir Guron <karanbir.GURON@htl.rennweg.at>
2024-03-02 23:09:59 +0100

    UE03: PI Plot version 0

---
+++ UE03_Plot/Main.py
@@ -0,0 +1,60 @@
+import matplotlib.pyplot as plt
+import numpy
+import math
+
+CNT = 1024
+PI = math.pi
+
+X = [-PI + ((2*PI)*i / (CNT-1)) for i in range(CNT)]
+C = [math.cos(x) for x in X]
+S = [math.sin(x) for x in X]
+
+def plot():
+    plt.figure(figsize=(10,6), dpi=80)
+    plt.xlim(min(X) * 1.1, max(X) * 1.1)
+    plt.ylim(min(C) * 1.1, max(C) * 1.1)
+    plt.xticks([-PI, -PI / 2, 0, PI / 2, PI],
+               [r'$-\pi$', r'$-\pi/2$', r'$0$', r'$+\pi/2$', r'$+\pi$'])
+    plt.yticks([-1, 0, +1],
+               [r'$-1$', r'$0$', r'$+1$'])
+
+    # Aufgabe 1 Linienart und Farbe
+    plt.plot(X,C, color="brown", linewidth=2.5, linestyle='-.', label="cosine")
+    plt.plot(X,S, color="green", linewidth=2.5, linestyle='-.', label="sine")
+
+    plt.legend(loc='upper left', frameon=False)
+
+    ax = plt.gca()
+    ax.spines['right'].set_color('none')
+    ax.spines['top'].set_color('none')
+    ax.xaxis.set_ticks_position('bottom')
+    ax.spines['bottom'].set_position(('data', 0))
+    ax.yaxis.set_ticks_position('left')
+    ax.spines['left'].set_position(('data', 0))
+
+    t = 2 * PI / 3
+    plt.plot([t, t], [0, math.cos(t)], color='brown', linewidth=2.5, linestyle="--")
+    plt.scatter([t, ], [math.cos(t), ], 50, color='brown')
+    plt.annotate(r'$\sin(\frac{2\pi}{3})=\frac{\sqrt{3}}{2}$',
+                 xy=(t, math.sin(t)), xycoords='data',
+                 xytext=(+10, +30), textcoords='offset points', fontsize=16,
+                 arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2"))
+
+    plt.plot([t, t], [0, math.sin(t)], color='green', linewidth=2.5, linestyle="--")
+    plt.scatter([t, ], [math.sin(t), ], 50, color='green')
+
+    plt.annotate(r'$\cos(\frac{2\pi}{3})=-\frac{1}{2}$',
+                 xy=(t, math.cos(t)), xycoords='data',
+                 xytext=(-90, -50), textcoords='offset points', fontsize=16,
+                 arrowprops=dict(arrowstyle="->", connectionstyle="arc3,rad=.2"))
+
+    for label in ax.get_xticklabels() + ax.get_yticklabels():
+        label.set_fontsize(16)
+    label.set_bbox(dict(facecolor='white', edgecolor='None', alpha=0.65))
+    ax.set_axisbelow(True)
+
+    plt.show()
+
+if __name__ == "__main__":
+    print(X)
+    plot()
